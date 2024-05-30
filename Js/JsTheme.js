177f1a7289f7fc00aa6e731030e4967ba4ffa4ca
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-05-29 22:56:35 -0300

    Update

--- Js/JsTheme.js
+++ Js/JsTheme.js
@@ -2,12 +2,8 @@ function setTheme(theme) {
     if (theme === 'dark') {
         mode.classList.remove('fa-moon');
         mode.classList.add('bxs-sun');
-        form.classList.add('dark');
-        form2.classList.add('select');
     } else {
         mode.classList.remove('bxs-sun');
         mode.classList.add('bxs-moon');
-        form.classList.remove('dark');
-        form2.classList.remove('select');
     }
 }
